da48a223c7f19a7c88c8e876ee3cc87dd38ca817
unknown <lazaltron@fecea.edu.ar>
2026-04-14 21:08:53 -0300

    serverexpress

--- EstoNoSeDondeIba/index.js
+++ EstoNoSeDondeIba/index.js
@@ -54,30 +54,3 @@ const port = 3000
 server.listen(port,() =>{
     console.log(`servidor escuchando en el puerto ${port}`)
 })
-
-const infoProductos = {
-    productos:[
-        {
-            id: 1,
-            descripcion: 'pantalon',
-            precio: 60000
-        },
-        {
-            id: 2,
-            descripcion: 'campera',
-            precio: 20000
-        },
-        {
-            id: 3,
-            descripcion: 'zapatillas',
-            precio: 130000
-        },
-        {
-            id: 4,
-            descripcion: 'remera',
-            precio: 5000
-        }                   
-    ]
-}
-
-module.exports.infoProductos = infoProductos
---
+++ EstoNoSeDondeIba/productos.js
@@ -0,0 +1,26 @@
+const infoProductos = {
+    productos:[
+        {
+            id: 1,
+            descripcion: 'pantalon',
+            precio: 60000
+        },
+        {
+            id: 2,
+            descripcion: 'campera',
+            precio: 20000
+        },
+        {
+            id: 3,
+            descripcion: 'zapatillas',
+            precio: 130000
+        },
+        {
+            id: 4,
+            descripcion: 'remera',
+            precio: 5000
+        }                   
+    ]
+}
+
+module.exports.infoProductos = infoProductos
